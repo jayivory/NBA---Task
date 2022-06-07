89f8c0aa8db99cbad0c5fb75db224fa54513d840
farhan <hay40853@adobe.com>
2022-06-07 13:16:37 +0530

    add infinite scroll

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroller";
 
 function Card() {
+
+  
   const [data, setData] = useState([]);
-  // console.log("statdata",data)
+  const [page, setPage] = useState(0)
+  console.log(page)
+  // console.log("statedata",data)
 
   const axios = require("axios");
 
   const options = {
     method: "GET",
     url: "https://free-nba.p.rapidapi.com/players",
-    params: { page: "0", per_page: "25" },
+    params: { page: page, per_page: "5" },
     headers: {
       "X-RapidAPI-Host": "free-nba.p.rapidapi.com",
       "X-RapidAPI-Key": "2fa92c71d8msh92e316aa65699bdp14a259jsnc05a48d9c2a1",
@@ -20,7 +25,7 @@ function Card() {
   axios
     .request(options)
     .then(function (response) {
-      setData(response.data.data);
+      setData([...data, ...response.data.data]);
     })
     .catch(function (error) {
       console.error(error);
@@ -28,12 +33,25 @@ function Card() {
 
   const HandleButton = () => {};
 
+  const scrollEnd = () => {
+    setPage(page + 1);
+  }
+
+  window.onscroll = () => {
+    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight)
+    {
+      scrollEnd();
+      // console.log("scrollB bottom")
+    }
+  } 
+
   return (
     <>
       {data.map(function (val) {
         {
-          {/* console.log(val.id) */}
-        
+          {
+            /* console.log(val.id) */
+          }
         }
         return (
           <div className="container">
@@ -45,7 +63,11 @@ function Card() {
                     <h5 clssName="fname">{val.first_name}</h5>
                     <h5 clssName="lname">{val.last_name}</h5>
                     <Link to={`CardData/${val.id}`}>
-                      <button type="" clssName="btn btn-success" onMouseOver={HandleButton}>
+                      <button
+                        type=""
+                        clssName="btn btn-success"
+                        onMouseOver={HandleButton}
+                      >
                         button
                       </button>
                     </Link>
